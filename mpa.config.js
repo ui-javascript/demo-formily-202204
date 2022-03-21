99438af60152855634db1728d0e7684c0f5c3468
luo0412 <1095847440@qq.com>
2022-03-21 09:11:39 +0800

    adjust

--- mpa.config.js
+++ mpa.config.js
@@ -2,5 +2,6 @@ exports.CONFIG = {
 //   entry: './_demo/**/*.{js,tsx}',
   // entry: './__demo/**/index.tsx',
   // entry: './_x-render/*.js',
-  entry: './_chart-render/*.js',
+  // entry: './_chart-render/*.js',
+  entry: './_form-render/*.js',
 }
